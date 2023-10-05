82a9849fa773a3271c408948f7909c8933e7df8a
Yuu Kruth <kyuu962@gmail.com>
2023-10-05 17:05:32 +0200

    implementing Piece class

--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -1,4 +1,19 @@
 package com.chess.engine.pieces;
 
-public class Piece {
+import com.chess.engine.Alliance;
+import com.chess.engine.board.Board;
+import com.chess.engine.board.Move;
+
+import java.util.List;
+
+public abstract class Piece {
+    protected final int piecePosition;
+    protected final Alliance pieceAlliance;
+
+    Piece(final int getPiecePosition, final Alliance pieceAlliance){
+        this.pieceAlliance = pieceAlliance;
+        this.piecePosition = getPiecePosition;
+    }
+
+    public abstract List<Move> calculateLegalMoves(final Board board);
 }
