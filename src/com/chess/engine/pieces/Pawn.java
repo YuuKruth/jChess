5fed2b26bb4be5439a7780a43b0e78d0fb78afa4
Yuu Kruth <kyuu962@gmail.com>
2023-10-06 14:15:21 +0200

    starting to implement the Pawn class

---
+++ src/com/chess/engine/pieces/Pawn.java
@@ -0,0 +1,36 @@
+package com.chess.engine.pieces;
+
+import com.chess.engine.Alliance;
+import com.chess.engine.board.Board;
+import com.chess.engine.board.BoardUtils;
+import com.chess.engine.board.Move;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Pawn extends Piece{
+
+    private final static int[] CANDIDATE_MOVE_COORDINATE = {8};
+    Pawn(int getPiecePosition, Alliance pieceAlliance) {
+        super(getPiecePosition, pieceAlliance);
+    }
+
+    @Override
+    public Collection<Move> calculateLegalMoves(final Board board) {
+
+        final List<Move> legalMoves = new ArrayList<>();
+        for (final int currentCandidateOffset : CANDIDATE_MOVE_COORDINATE) {
+            final int candidateDestinationCoordinate = this.piecePosition + (this.getPieceAlliance().getDirection() * currentCandidateOffset);
+            if (!BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)){
+                continue;
+            }
+            if (currentCandidateOffset == 8 && !board.getTile(candidateDestinationCoordinate).isTileOccupied()){
+                //TODO more work here!!!!
+                legalMoves.add(new Move.MajorMove(board, this, candidateDestinationCoordinate));
+            }
+        }
+        
+        return legalMoves;
+    }
+}
