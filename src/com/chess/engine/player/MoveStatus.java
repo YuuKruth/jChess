472689e42d31a3d4c8df67cf215b9fea010df577
Yuu Kruth <kyuu962@gmail.com>
2023-10-08 01:36:22 +0200

    implementing the Player-methods

--- src/com/chess/engine/player/MoveStatus.java
+++ src/com/chess/engine/player/MoveStatus.java
@@ -1,4 +1,12 @@
 package com.chess.engine.player;
 
 public enum MoveStatus {
+    DONE {
+        @Override
+        public boolean isDone() {
+            return true;
+        }
+    };
+
+    public abstract boolean isDone();
 }
--- src/com/chess/engine/player/Player.java
+++ src/com/chess/engine/player/Player.java
@@ -5,13 +5,17 @@ import com.chess.engine.board.Board;
 import com.chess.engine.board.Move;
 import com.chess.engine.pieces.King;
 import com.chess.engine.pieces.Piece;
+import com.google.common.collect.ImmutableList;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public abstract class Player {
     protected final Board board;
     protected final King playerKing;
     protected final Collection<Move> legalMoves;
+    private final boolean isInCheck;
 
     public Player(final Board board,
                   final Collection<Move> legalMoves,
@@ -19,6 +23,17 @@ public abstract class Player {
         this.board = board;
         this.playerKing = establishKing();
         this.legalMoves = legalMoves;
+        this.isInCheck = !Player.calculateAttacksOnTile(this.playerKing.getPiecePosition(), opponentMoves).isEmpty();
+    }
+
+    private static Collection<Object> calculateAttacksOnTile(int piecePosition, Collection<Move> moves) {
+        final List<Move> attackMoves = new ArrayList<>();
+        for (final Move move : moves) {
+            if (piecePosition == move.getDestinationCoordinate()) {
+                attackMoves.add(move);
+            }
+        }
+        return ImmutableList.copyOf(attackMoves);
     }
 
 
@@ -34,20 +49,31 @@ public abstract class Player {
     public boolean isMoveLegal(final Move move) {
         return this.legalMoves.contains(move);
     }
+
     //TODO implement these methods below!
     public boolean isInCheck() {
-        return false;
+        return this.isInCheck;
     }
 
     public boolean isInCheckMate() {
-        return false;
+        return this.isInCheck && !hasEscapeMoves();
     }
 
     public boolean isInStaleMate() {
-        return false;
+        return !this.isInCheck && !hasEscapeMoves();
     }
 
     public boolean isCastled() {
+        return false;
+    }
+
+    protected boolean hasEscapeMoves() {
+        for (final Move move : this.legalMoves) {
+            final MoveTransition transition = makeMove(move);
+            if (transition.getMoveStatus().isDone()) {
+                return true;
+            }
+        }
         return false;
     }
 
